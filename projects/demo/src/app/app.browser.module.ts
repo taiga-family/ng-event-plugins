c733c7e69a7201f7c3948d54e4fa7077d6f181c1
splincode <omaxphp@yandex.ru>
2024-06-11 17:53:11 +0300

    chore: fixup

--- projects/demo/src/app/app.browser.module.ts
+++ projects/demo/src/app/app.browser.module.ts
@@ -1,12 +1,12 @@
 import {APP_BASE_HREF, LocationStrategy, PathLocationStrategy} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {NG_EVENT_PLUGINS} from '@tinkoff/ng-event-plugins';
 import {HIGHLIGHT_OPTIONS, HighlightModule} from 'ngx-highlightjs';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app.routes';
 import {StaticModule} from './modules/static/static.module';
-import {NG_EVENT_PLUGINS} from "@tinkoff/ng-event-plugins";
 
 @NgModule({
     imports: [
