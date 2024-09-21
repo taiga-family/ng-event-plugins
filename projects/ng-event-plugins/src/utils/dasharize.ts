1d987f93acaf58860b5cc6c213bcc57284b0e7c7
splincode <omaxphp@yandex.ru>
2024-09-21 15:50:58 +0300

    chore: remove `dasharize`

--- projects/ng-event-plugins/src/utils/dasharize.ts
+++
@@ -1,6 +0,0 @@
-export function dasharize(camel: string): string {
-    return camel.replaceAll(
-        /[a-z][A-Z]/g,
-        (letterLetter) => `${letterLetter[0]}-${letterLetter[1].toLowerCase()}`,
-    );
-}
