e6a2df99b0449adc54df00aa9712be805b19f99d
splincode <omaxphp@yandex.ru>
2024-10-11 11:07:58 +0300

    chore: fixup regex for ios

--- projects/ng-event-plugins/src/utils/is-ios.ts
+++ projects/ng-event-plugins/src/utils/is-ios.ts
@@ -1,3 +1,3 @@
 export const isIos = ({userAgent, maxTouchPoints}: Navigator): boolean =>
-    /ipad|iphone|ipod|mac/i.test(userAgent) ||
+    /ipad|iphone|ipod/i.test(userAgent) ||
     (/^((?!chrome|android).)*safari/i.test(userAgent) && maxTouchPoints > 1);
